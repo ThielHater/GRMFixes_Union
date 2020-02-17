0538a4e7d72b043b280e03b5b97a6ea539f96d51
ThielHater <pierre.beckmann@yahoo.de>
2020-02-17 22:25:03 +0100

    printf

--- XZenFileFormat/XZenFileFormat/Plugin_Source.hpp
+++ XZenFileFormat/XZenFileFormat/Plugin_Source.hpp
@@ -69,44 +69,47 @@ namespace NAMESPACE {
 			return; // Already patched
 
 		// Indices are only 2 bytes in Gothic 1. Patch some bytes to bump that to 4.
+		printf("Patching Game...\n");
 
 		// SaveMSH: push 2 -> push 4  -- fwrite-size
 		const char* push4 = "\x6A\x04";
 		ReplaceCodeBytes(push4, strlen(push4), GothicMemoryLocations::SaveMSH::ASM_INDEX_SIZE_PUSH);
-		//debugPrint(" - push 2 -> push 4\n");
+		printf(" - push 2 -> push 4\n");
 
 		// SaveMSH: mov ax, [edx+0Ch] -> mov eax, [edx+0x0C] -- Cast index to 16-bit
 		// Also appended a 0x90 (nop) at the end, since the former instruction is only 3 bytes long.
 		const char* cast32 = "\x8B\x42\x0C\x90";
 		ReplaceCodeBytes(cast32, strlen(cast32), GothicMemoryLocations::SaveMSH::ASM_INDEX_CAST);
-		//debugPrint(" - mov ax, [edx+0Ch] -> mov eax, [edx+0x0C]\n");
+		printf(" - mov ax, [edx+0Ch] -> mov eax, [edx+0x0C]\n");
 
 		// LoadMSH: add [esp+214h+var_1FC], 6 -> add [esp+214h+var_1FC], 8 -- Offset to the next vertex/feature-index pair. 
 		// Feature-Indices are 4bytes, so with a 2byte vertexindex it gives 6. We need to make it 8.
 		const char* offset8 = "\x83\x44\x24\x18\x08";
 		ReplaceCodeBytes(offset8, strlen(offset8), GothicMemoryLocations::LoadMSH::ASM_INDEX_STRUCT_SIZE_ADD);
-		//debugPrint(" - add [esp+214h+var_1FC], 6 -> add [esp+214h+var_1FC], 8\n");
+		printf(" - add [esp+214h+var_1FC], 6 -> add [esp+214h+var_1FC], 8\n");
 
 		// LoadMSH: movzx eax, word ptr [edx] -> mov eax, [edx] -- Remove the cast from 32 to 16-bit when getting the index out of the datastructure
 		// Also add a NOP, since a regular mov is only 2 bytes instead of 3 for movzx.
 		const char* loadCast = "\x8B\x02\x90";
 		ReplaceCodeBytes(loadCast, strlen(loadCast), GothicMemoryLocations::LoadMSH::ASM_INDEX_CAST);
-		//debugPrint(" - movzx eax, word ptr [edx] -> mov eax, [edx]\n");
+		printf(" - movzx eax, word ptr [edx] -> mov eax, [edx]\n");
 
 		// LoadBIN: Mute the annoying warning about a "too large" chunk
 		ReplaceCodeRange(0x90, GothicMemoryLocations::LoadBin::ASM_BSP_ERROR_START, GothicMemoryLocations::LoadBin::ASM_BSP_ERROR_END);
-		//debugPrint(" - Muted \"Too large chunk\"-warning on ZEN-Load.\n");
+		printf(" - Muted \"Too large chunk\"-warning on ZEN-Load.\n");
 
 		// LoadMSH: mov esi, [ecx+2] -> mov esi, [ecx+4] -- Need to offset the feature by 4, not by 2
 		const char* featOffset = "\x8B\x71\x04";
 		ReplaceCodeBytes(featOffset, strlen(featOffset), GothicMemoryLocations::LoadMSH::ASM_FEATINDEX_OFFSET);
-		//debugPrint(" - mov esi, [ecx+2] -> mov esi, [ecx+4]\n");
+		printf(" - mov esi, [ecx+2] -> mov esi, [ecx+4]\n");
 
 		// LoadMSH: lea edx, [eax+eax*2] -> imul edx, eax, 4 -- In the next line, edx will be multiplyed by 2, so since edx = 3 * edx it becomes 6 * eax in the next line.
 		// Replace this with code that multiplies eax by 4 so we get 8 with the next line.
 		const char* imul = "\x6B\xD0\x04";
 		ReplaceCodeBytes(imul, strlen(imul), GothicMemoryLocations::LoadMSH::ASM_BLOCK_OFFSET_LEA_FIRST);
-		//debugPrint(" - lea edx, [eax+eax*2] -> imul edx, eax, 4\n");
+		printf(" - lea edx, [eax+eax*2] -> imul edx, eax, 4\n");
+
+		printf("Done!\n");
 	}
 
 	// 0x00509A40 private: void __thiscall zCArchiverFactory::ReadLineArg(class zSTRING &,class zSTRING &,class zCBuffer *,class zFILE *)
@@ -140,6 +143,7 @@ namespace NAMESPACE {
 		// Apply fixes if the file is right
 		if (isLoadingXZEN)
 		{
+			printf("Loading an enhanced ZEN. Applying code modifications.\n");
 			FixZENLoadSave();
 		}
 
@@ -149,6 +153,7 @@ namespace NAMESPACE {
 		// Reset everything, worldmesh is done
 		if (isLoadingXZEN)
 		{
+			printf("Done loading Mesh! Restoring original code... \n");
 			isLoadingXZEN = false;
 
 			// Go back to normal at the start
