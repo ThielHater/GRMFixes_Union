01f78df957a3edf68eeef4586e109e56a15cc582
ThielHater <pierre.beckmann@yahoo.de>
2023-10-07 23:26:53 +0200

    Use Bink to play video

--- GRMFixes/AnimatedStartscreen/Plugin_Source.hpp
+++ GRMFixes/AnimatedStartscreen/Plugin_Source.hpp
@@ -3,49 +3,99 @@
 // For compile plugin in Multiplatform mode - select 'Debug' or 'Release' configuration.
 
 #include "Plugin_Header.h"
+#include "bink.h"
 
 namespace NAMESPACE
 {
-	void Init()
-	{
-		std::thread animThread(Animate);
-		animThread.detach();
-	}
+    #define zRES_CACHE_LOCKED 2
 
-	void Animate()
-	{
-		int curFrame = 0;
-		int maxFrame = 0;
+    void Init()
+    {
+        std::thread animThread(Animate);
+        animThread.detach();
+    }
 
-		do
-		{
-			zCTexture* tex = zCTexture::Load(zSTRING("STARTSCREEN_A") + zSTRING(maxFrame) + zSTRING(".TGA"), FALSE);
+    void Animate()
+    {
+        HMODULE dllBink = GetModuleHandleA("binkw32");
+        if (!dllBink)
+            return;
 
-			zCOLOR avg = tex->GetTextureInfo().averageColor;
+        PFN_BinkOpen BinkOpen = (PFN_BinkOpen)GetProcAddress(dllBink, "_BinkOpen@8");
+        PFN_BinkClose BinkClose = (PFN_BinkClose)GetProcAddress(dllBink, "_BinkClose@4");
+        PFN_BinkGoto BinkGoto = (PFN_BinkGoto)GetProcAddress(dllBink, "_BinkGoto@12");
+        PFN_BinkDoFrame BinkDoFrame = (PFN_BinkDoFrame)GetProcAddress(dllBink, "_BinkDoFrame@4");
+        PFN_BinkNextFrame BinkNextFrame = (PFN_BinkNextFrame)GetProcAddress(dllBink, "_BinkNextFrame@4");
+        PFN_BinkWait BinkWait = (PFN_BinkWait)GetProcAddress(dllBink, "_BinkWait@4");
+        PFN_BinkCopyToBuffer BinkCopyToBuffer = (PFN_BinkCopyToBuffer)GetProcAddress(dllBink, "_BinkCopyToBuffer@28");
+        PFN_BinkOpenWaveOut BinkOpenWaveOut = (PFN_BinkOpenWaveOut)GetProcAddress(dllBink, "_BinkOpenWaveOut@4");
+        PFN_BinkSetSoundSystem BinkSetSoundSystem = (PFN_BinkSetSoundSystem)GetProcAddress(dllBink, "_BinkSetSoundSystem@8");
+        PFN_BinkSetSoundOnOff BinkSetSoundOnOff = (PFN_BinkSetSoundOnOff)GetProcAddress(dllBink, "_BinkSetSoundOnOff@8");
+        PFN_BinkSetVolume BinkSetVolume = (PFN_BinkSetVolume)GetProcAddress(dllBink, "_BinkSetVolume@8");
 
-			if (avg.dword == 0xff29241d || avg.dword == 0xff800080 || avg.dword == 0) // check for default texture
-			{
-				maxFrame--;
-				break;
-			}
-			else
-			{
-				maxFrame++;
-			}
-		}
-		while (true);
+        zSTRING rootDir = zoptions->GetDirString(DIR_ROOT);
+        zSTRING videoDir = zoptions->GetDirString(DIR_VIDEO);
+        zSTRING videoFile = zSTRING("STARTSCREEN.BIK");
 
-		while (maxFrame > 0 && !gameMan->IsGameRunning() && !gameMan->exitGame)
-		{
-			gameMan->initScreen->InsertBack(zSTRING("STARTSCREEN_A") + zSTRING(curFrame) + zSTRING(".TGA"));
+        BinkSetSoundSystem(BinkOpenWaveOut, 0);
 
-			auto timePoint = std::chrono::steady_clock::now() + std::chrono::milliseconds(40); // 25 fps
-			std::this_thread::sleep_until(timePoint);
+        BINK* bink = BinkOpen(rootDir + videoDir + videoFile, 0);
+        if (!bink)
+            return;
 
-			if (curFrame < maxFrame)
-				curFrame++;
-			else
-				curFrame = 0;
-		}
-	}
+        if (bink->NumTracks > 0)
+        {
+            BinkSetSoundOnOff(bink, 1);
+            BinkSetVolume(bink, 0, 65535);
+        }
+
+        zCTexture* tex = zrenderer->CreateTexture();
+        gameMan->initScreen->InsertBack(tex);
+
+        while (!gameMan->IsGameRunning() && !gameMan->exitGame)
+        {
+            // TODO: How to stop music earlier?
+            if (bink->NumTracks > 0)
+                zmusic->Stop();
+
+            BinkDoFrame(bink);
+            BinkNextFrame(bink);
+
+            // wait until it is time to show the frame
+            while (true)
+            {
+                if (!BinkWait(bink))
+                    break;
+            }
+
+            zCTextureConvert* texConv = zrenderer->CreateTextureConvert();
+
+            zCTextureInfo texInfo;
+            // Gothic and Bink have to use the same format!
+            // As RGB textures are not supported in Gothic, this may be the best option.
+            texInfo.texFormat = zRND_TEX_FORMAT_RGB_565;
+            texInfo.sizeX = bink->Width;
+            texInfo.sizeY = bink->Height;
+            texInfo.numMipMap = 1;
+            texConv->SetTextureInfo(texInfo);
+
+            texConv->Lock(zRES_CACHE_LOCKED);
+
+            BYTE* texBuffer;
+            int pitch; // width * bytes per pixel
+            texConv->GetTextureBuffer(0, (void*&)texBuffer, pitch);
+
+            BinkCopyToBuffer(bink, texBuffer, pitch, bink->Height, 0, 0, BINKCOPYALL | BINKSURFACE565);
+
+            zCTextureExchange::CopyContents(texConv, tex);
+
+            texConv->Unlock();
+            delete texConv;
+
+            if (bink && bink->FrameNum > bink->Frames)
+                BinkGoto(bink, 1, 0);
+        }
+
+        BinkClose(bink);
+    }
 }
---
+++ GRMFixes/AnimatedStartscreen/bink.h
@@ -0,0 +1,85 @@
+// minified bink.h from Bink SDK 1.5.21
+
+typedef struct BINKRECT {
+	int Left, Top, Width, Height;
+} BINKRECT;
+
+#define BINKMAXDIRTYRECTS 8
+
+typedef struct BINK {
+	unsigned int Width; // Width (1 based, 640 for example)
+	unsigned int Height; // Height (1 based, 480 for example)
+	unsigned int Frames; // Number of frames (1 based, 100 = 100 frames)
+	unsigned int FrameNum; // Frame to *be* displayed (1 based)
+	unsigned int LastFrameNum; // Last frame decompressed or skipped (1 based)
+
+	unsigned int FrameRate; // Frame Rate Numerator
+	unsigned int FrameRateDiv; // Frame Rate Divisor (frame rate=numerator/divisor)
+
+	unsigned int ReadError; // Non-zero if a read error has ocurred
+	unsigned int OpenFlags; // flags used on open
+	unsigned int BinkType; // Bink flags
+
+	unsigned int Size; // size of file
+	unsigned int FrameSize; // The current frame's size in bytes
+	unsigned int SndSize; // The current frame sound tracks' size in bytes
+
+	BINKRECT FrameRects[BINKMAXDIRTYRECTS]; // Dirty rects from BinkGetRects
+	int NumRects;
+
+	unsigned int PlaneNum; // which set of planes is current
+	void* YPlane[2]; // pointer to the uncompressed Y (Cr and Cr follow)
+	void* APlane[2]; // decompressed alpha plane (if present)
+	unsigned int YWidth; // widths and heights of the video planes
+	unsigned int YHeight;
+	unsigned int UVWidth;
+	unsigned int UVHeight;
+
+	void* MaskPlane; // pointer to the mask plane (Ywidth/16*Yheight/16)
+	unsigned int MaskPitch; // Mask Pitch
+	unsigned int MaskLength; // total length of the mask plane
+
+	unsigned int LargestFrameSize; // Largest frame size
+	unsigned int InternalFrames; // how many frames were potentially compressed
+
+	int NumTracks; // how many tracks
+
+	unsigned int Highest1SecRate; // Highest 1 sec data rate
+	unsigned int Highest1SecFrame; // Highest 1 sec data rate starting frame
+
+	int Paused; // is the bink movie paused?
+
+	unsigned int BackgroundThread; // handle to background thread
+} BINK;
+
+#define BINKCOPYALL 0x80000000L // copy all pixels (not just changed)
+
+#define BINKSURFACE8P    0
+#define BINKSURFACE24    1
+#define BINKSURFACE24R   2
+#define BINKSURFACE32    3
+#define BINKSURFACE32R   4
+#define BINKSURFACE32A   5
+#define BINKSURFACE32RA  6
+#define BINKSURFACE4444  7
+#define BINKSURFACE5551  8
+#define BINKSURFACE555   9
+#define BINKSURFACE565  10
+#define BINKSURFACE655  11
+#define BINKSURFACE664  12
+#define BINKSURFACEYUY2 13
+#define BINKSURFACEUYVY 14
+#define BINKSURFACEYV12 15
+#define BINKSURFACEMASK 15
+
+typedef BINK* (WINAPI* PFN_BinkOpen)(const char* filePath, unsigned int flags);
+typedef void (WINAPI* PFN_BinkClose)(void* bink);
+typedef void (WINAPI* PFN_BinkGoto)(void* bink, unsigned int frame, int flags);
+typedef unsigned int (WINAPI* PFN_BinkDoFrame)(void* bink);
+typedef void (WINAPI* PFN_BinkNextFrame)(void* bink);
+typedef int (WINAPI* PFN_BinkWait)(void* bink);
+typedef int (WINAPI* PFN_BinkCopyToBuffer)(void* bink, void* dest, int destPitch, unsigned int destHeight, unsigned int destX, unsigned int destY, unsigned int flags);
+typedef void* (WINAPI* PFN_BinkOpenWaveOut)(unsigned int flags);
+typedef int (WINAPI* PFN_BinkSetSoundSystem)(void* open, HANDLE param);
+typedef unsigned int (WINAPI* PFN_BinkSetSoundOnOff)(void* bink, int onOff);
+typedef void (WINAPI* PFN_BinkSetVolume)(void* bink, unsigned int trackId, int volume);
